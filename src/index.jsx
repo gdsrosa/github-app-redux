4215e1b258ef875748aadf493003c79ebd575c6d
Gabriel Rosa <gabrieldsrosa@gmail.com>
2025-01-27 18:41:00 +0000

    feat: upgrade project dependencies

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
+import { createRoot } from 'react-dom/client';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
 import App from './App.jsx';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
